dd0ec5caf7445280ab6173f1416952a693feccba
sheetsgeogle <vhsv3576@gmail.com>
2024-07-08 12:03:47 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 import streamlit as st
-from fpdf import FPDF
+import pdfkit
+import requests
 
 # Title of the app
-st.title('Sefaria Psalms Link Generator')
+st.title('Sefaria Psalms Link Generator and PDF Creator')
 
 # Prompt the user to enter a number
 number = st.number_input('Enter a Psalm number:', min_value=1, max_value=150, step=1)
@@ -19,16 +20,17 @@ st.markdown(f'[Click here to view Psalm {number}]({link})')
 
 # Button to create the PDF
 if st.button('Generate PDF'):
-    # Create PDF
-    pdf = FPDF()
-    pdf.add_page()
-    pdf.set_font('Arial', size=12)
-    pdf.cell(200, 10, txt=f'Sefaria Psalms Link for Psalm {number}', ln=True, align='C')
-    pdf.cell(200, 10, txt=link, ln=True, align='C')
+    # Fetch content from the link
+    response = requests.get(link)
+    html_content = response.text
 
-    # Save PDF to a file
+    # Save the content to an HTML file
+    with open('temp.html', 'w', encoding='utf-8') as file:
+        file.write(html_content)
+
+    # Convert HTML file to PDF
     pdf_output = f'Sefaria_Psalms_{number}.pdf'
-    pdf.output(pdf_output)
+    pdfkit.from_file('temp.html', pdf_output)
 
     # Provide a link to download the PDF
     with open(pdf_output, 'rb') as pdf_file:
