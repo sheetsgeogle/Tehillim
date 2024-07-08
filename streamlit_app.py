657ba29f80dce96696c861dea45920ca6ff2f48d
sheetsgeogle <vhsv3576@gmail.com>
2024-07-08 12:02:06 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from fpdf import FPDF
 
 # Title of the app
 st.title('Sefaria Psalms Link Generator')
@@ -15,3 +16,20 @@ st.write(link)
 
 # Provide a clickable link
 st.markdown(f'[Click here to view Psalm {number}]({link})')
+
+# Button to create the PDF
+if st.button('Generate PDF'):
+    # Create PDF
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font('Arial', size=12)
+    pdf.cell(200, 10, txt=f'Sefaria Psalms Link for Psalm {number}', ln=True, align='C')
+    pdf.cell(200, 10, txt=link, ln=True, align='C')
+
+    # Save PDF to a file
+    pdf_output = f'Sefaria_Psalms_{number}.pdf'
+    pdf.output(pdf_output)
+
+    # Provide a link to download the PDF
+    with open(pdf_output, 'rb') as pdf_file:
+        st.download_button('Download PDF', pdf_file, file_name=pdf_output)
