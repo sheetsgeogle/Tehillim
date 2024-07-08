4093c0395f2224bd7a1db80814c4f7706f0bdb29
sheetsgeogle <vhsv3576@gmail.com>
2024-07-08 12:07:19 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,15 @@
 import streamlit as st
-import pdfkit
 import requests
-import os
+from pyppeteer import launch
+import asyncio
+
+# Function to generate PDF using pyppeteer
+async def generate_pdf(url, output_path):
+    browser = await launch()
+    page = await browser.newPage()
+    await page.goto(url, {'waitUntil': 'networkidle0'})
+    await page.pdf({'path': output_path, 'format': 'A4'})
+    await browser.close()
 
 # Title of the app
 st.title('Sefaria Psalms Link Generator and PDF Creator')
@@ -21,22 +29,11 @@ st.markdown(f'[Click here to view Psalm {number}]({link})')
 
 # Button to create the PDF
 if st.button('Generate PDF'):
-    # Fetch content from the link
-    response = requests.get(link)
-    html_content = response.text
-
-    # Save the content to an HTML file
-    with open('temp.html', 'w', encoding='utf-8') as file:
-        file.write(html_content)
-
-    # Convert HTML file to PDF
     pdf_output = f'Sefaria_Psalms_{number}.pdf'
-    pdfkit.from_file('temp.html', pdf_output)
+    
+    # Run the async function to generate PDF
+    asyncio.get_event_loop().run_until_complete(generate_pdf(link, pdf_output))
 
     # Provide a link to download the PDF
     with open(pdf_output, 'rb') as pdf_file:
         st.download_button('Download PDF', pdf_file, file_name=pdf_output)
-
-    # Clean up temporary files
-    os.remove('temp.html')
-    os.remove(pdf_output)
