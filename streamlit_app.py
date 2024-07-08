bfb96172eb26ec5237f362694703b4833e7b0aa6
sheetsgeogle <vhsv3576@gmail.com>
2024-07-08 12:00:41 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,17 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# Title of the app
+st.title('Sefaria Psalms Link Generator')
+
+# Prompt the user to enter a number
+number = st.number_input('Enter a Psalm number:', min_value=1, max_value=150, step=1)
+
+# Generate the link
+link = f'https://www.sefaria.org/Psalms.{number}?lang=he'
+
+# Display the link
+st.write('Generated Link:')
+st.write(link)
+
+# Provide a clickable link
+st.markdown(f'[Click here to view Psalm {number}]({link})')
